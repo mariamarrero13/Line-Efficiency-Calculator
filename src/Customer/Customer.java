450e4e6ff61f2273767cf4ae9a50d3e32f8c408c
Maria Alejandra Marrero <maria.marrero13@upr.edu>
2018-04-21 16:19:58 -0400

    modified customer class

--- src/Customer/Customer.java
+++ src/Customer/Customer.java
@@ -1,6 +1,40 @@
 package Customer;
 
 public class Customer {
-	
+	private int custid;         // id of this customer
+	private int serverid; 	   // id of the line the customer is
+	private int arrivalTime;    // arrival time of this customer
+	private int remainingTime;  // remaining service time for this customer
+	public Customer (int id, int at, int rt) { 
+		custid = id; 
+		arrivalTime = at; 
+		remainingTime = rt; 
+	}
+	public int getCustid() {
+		return custid;
+	}
+	public int getServerid() {
+		return serverid;
+	}
+	public void setServerid(int id) {
+		serverid = id;
+	}
+	public int getArrivalTime() {
+		return arrivalTime;
+	}
+	public int getRemainingTime() {
+		return remainingTime;
+	}
+	/**
+	 * Registers an update of serviced received by this customer. 
+	 * @param q the time of the service being registered. 
+	 */
+	public void isServed(int q) { 
+		if (q < 0) return;   // only register positive value of q
+		remainingTime -= q; 
+	}
+	public boolean isServiceCompleted() {
+		return remainingTime==0;
+	}
 
 }
