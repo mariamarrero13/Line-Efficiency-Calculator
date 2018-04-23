b6302717dfc30271949a06b77b478ddf9656bffd
Maria Alejandra Marrero <maria.marrero13@upr.edu>
2018-04-22 22:42:37 -0400

    I had an old version of data reader. Fixed it

--- src/DataReader/DataReader.java
+++ src/DataReader/DataReader.java
@@ -2,16 +2,18 @@ package DataReader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import Customer.Customer;
 import java.util.Deque;
+import java.util.Queue;
 
 public class DataReader {
 
-	private static ArrayList<String> readData() throws FileNotFoundException {
+	private static ArrayList<String> readFileNames() throws FileNotFoundException {
 		ArrayList<String> data = new ArrayList<String>();
 		String fileName = "dataFiles.txt"; 
 		Scanner inputFile = new Scanner(new File("src",fileName)); 
@@ -21,15 +23,23 @@ public class DataReader {
 		inputFile.close();
 		return fileContent; 
 	}
-	private static Deque<Customer> arraytoQueue(ArrayList<String> data) {
-		Deque<Customer> inputDeque = new ArrayDeque<Customer>();
-		for (String s : data){
-			String[] inputs = s.split(",");
-			Customer nc = new Customer(data.indexOf(s), Integer.valueOf(inputs[0]), Integer.valueOf(inputs[1]));
-			inputDeque.add(nc);
+	private static ArrayList<Queue> readData() throws FileNotFoundException{
+		ArrayList<String> fileNames = readFileNames();
+		ArrayList<Queue> files = new ArrayList<Queue>();
+		for (String fileName : fileNames) {
+			Scanner inputFile = new Scanner(new File("src",fileName));
+			ArrayList<String> fileContent = new ArrayList<>(); 
+			while (inputFile.hasNext())
+				fileContent.add(inputFile.nextLine());
+			inputFile.close();
+			Queue<Customer> cd = new ArrayDeque<>();
+			for (String s : fileContent){
+				String[] inputs = s.split(",");
+				Customer nc = new Customer(fileNames.indexOf(s), Integer.valueOf(inputs[0]), Integer.valueOf(inputs[1]));
+				cd.add(nc);
+			}
+			files.add(cd);
 		}
-
-		return inputDeque;
+		return files;
 	}
-	
 }
